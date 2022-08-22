acfd5bfc0b240f5c2d6f26b6289b1e3ccba14b8c
simon <simon_brandt@gmx.de>
2022-08-22 17:31:31 +0200

    INSTALL DEPENDENCIES

--- mockServer/index.js
+++ mockServer/index.js
@@ -1,9 +1,7 @@
 require = require("esm")(module/*, options*/) // enables es6 modules in node.js
 
-const runSignalingServer = require('./signalingServer')
 const runRestServer = require('./restServer')
 const runClientStateServer = require('./clientStateServer')
 
-runSignalingServer()
 runRestServer()
 runClientStateServer()
