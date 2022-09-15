c95fa5c930e8baa0949bc1705f3c9621d2aabcf1
simon <simon_brandt@gmx.de>
2022-09-15 14:19:43 +0200

    WORK on icon implementation

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,46 @@
 <script setup lang="ts">
 import {onMounted} from 'vue'
 import {useActivitiesStore} from 'stores/activities-store'
+import {useConfigStore} from 'stores/config-store'
+import {useQuasar} from 'quasar'
 
+const $q = useQuasar()
+
+const myIcons = {
+  'app:actor': 'img:/src/assets/entities/actor.svg',
+  'app:event': 'img:/src/assets/entities/event.svg',
+}
+
+// Example of adding support for
+// <q-icon name="app:...." />
+// This includes support for all "icon" props
+// of Quasar components
+
+$q.iconMapFn = (iconName) => {
+  // iconName is the content of QIcon "name" prop
+
+  // your custom approach, the following
+  // is just an example:
+  $q.iconMapFn = (iconName) => {
+    const icon = myIcons[iconName]
+    if (icon !== void 0) {
+      return { icon: icon }
+    }
+  }
+
+  // when we don't return anything from our
+  // iconMapFn, the default Quasar icon mapping
+  // takes over
+}
+
+$q.iconMapFn = null
+
+
+const configStore = useConfigStore()
 const activitiesStore = useActivitiesStore()
+
 onMounted(() => {
+  configStore.getConfig()
   activitiesStore.getActivities()
 })
 
---
+++ src/components/filters/entity-icons/EntityIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="entity-icon">
+    <slot></slot>
+  </div>
+</template>
+<script setup>
+import {defineProps, onUpdated} from 'vue'
+import {useConfigStore} from 'src/stores/config-store'
+
+const brandColor = useConfigStore().config.brandColor
+
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  }
+})
+
+onUpdated(() => {
+  const svgCircle = document.querySelector('.entity-icon svg circle')
+  svgCircle.style.fill = props.active ? brandColor : '#D9D9D9'
+})
+</script>
