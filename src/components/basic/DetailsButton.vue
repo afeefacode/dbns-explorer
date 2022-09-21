2c5e70f46124d78af398bc28af91738d9481599c
simon <simon_brandt@gmx.de>
2022-09-21 16:54:31 +0200

    ADD DetailsButton, MapListToggle

---
+++ src/components/basic/DetailsButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-btn :style="`background: ${config.brandColor}; color: ${config.brandTextColor}`"
+         :icon="`img:/src/assets/svg/${entity.type}.svg`"
+         :label="`${entity.type} anzeigen`"/>
+</template>
+<script setup>
+import {defineProps} from 'vue'
+import {useConfigStore} from 'src/stores/config-store'
+
+const props = defineProps({
+  entity: {
+    type: Object,
+    default: null
+  }
+})
+
+const config = useConfigStore().config
+</script>
---
+++ src/components/basic/MapListToggle.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="map-list-toggle">
+    <div
+      @click="$emit('view-toggled', 'map')"
+      :class="{active: activeView === 'map'}"
+    >
+      Karte
+    </div>
+    <div
+      @click="$emit('view-toggled', 'map')"
+      :class="{active: activeView === 'list'}"
+    >
+      Liste
+    </div>
+  </div>
+</template>
+<script setup>
+import {defineProps} from 'vue'
+import {useConfigStore} from "stores/config-store";
+
+const config = useConfigStore().config
+const props = defineProps({
+  activeView: {
+    type: String,
+  }
+})
+
+</script>
+<style lang="scss" scoped>
+.map-list-toggle {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-bottom: 2em;
+  cursor: pointer;
+
+  div {
+    color: #333;
+    background-color: #F8F8F8;
+    width: 200px;
+    text-align: center;
+    padding: .2em;
+  }
+
+  .active {
+    color: white;
+    background-color: var(--primary)
+  }
+}
+</style>
