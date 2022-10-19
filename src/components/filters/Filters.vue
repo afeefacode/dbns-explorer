2dfb6dd2da0fef1dc556d602f7f09469ffc51f40
simon <simon_brandt@gmx.de>
2022-10-19 14:43:05 +0200

    REFACTOR EntitySelector into MainLayout

--- src/components/filters/Filters.vue
+++ src/components/filters/Filters.vue
@@ -1,42 +1,19 @@
 <template>
   <div class="filters">
-    <div class="row entity-icons">
-      <EntitySelector
-        v-for="entity in config.entities"
-        :entity-name="entity.name"
-        :active="entity.name === activeEntity"
-        @entity-click="activeEntity = entity.name"
-      />
-    </div>
     <!--  <RegionFilter/>-->
     <!--  <CategoryFilter/>-->
   </div>
 </template>
 
 <script setup lang="ts">
-import {onUpdated, ref} from 'vue'
-import {useConfigStore} from 'src/stores/config-store'
-import EntitySelector from 'components/filters/EntitySelector.vue'
+// import {useConfigStore} from 'src/stores/config-store'
 // import CategoryFilter from 'components/filters/CategoryFilter.vue';
 // import RegionFilter from 'components/filters/RegionFilter.vue';
-
-const config = useConfigStore().config
-const activeEntity = ref('actor')
-
-const entityClicked = (entityName: string) => {
-}
-
-onUpdated(()=> {
-})
-
+// const config = useConfigStore().config
 </script>
 
 <style lang="scss" scoped>
 .filters {
   margin-bottom: 2em;
-
-  .entity-icons {
-    justify-content: center;
-  }
 }
 </style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,33 @@
 <template>
   <q-layout view="lHh Lpr lFf">
+    <div class="row entity-icons">
+      <EntitySelector
+        v-for="entity in config.entities"
+        :entity-name="entity.name"
+        :active="entity.name === activeEntity"
+        @entity-click="activeEntity = entity.name"
+      />
+    </div>
     <q-page-container class="page-container">
       <router-view/>
     </q-page-container>
   </q-layout>
 </template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+import {useConfigStore} from "src/stores/config-store";
+import EntitySelector from 'src/components/filters/EntitySelector.vue'
+
+const config = useConfigStore().config
+const activeEntity = ref('actor')
+</script>
+
 <style>
+.entity-icons {
+  justify-content: center;
+}
+
 .page-container {
   max-width: 1170px;
   margin: auto;
