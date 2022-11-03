34c91b55a3e0bdd10f5b1027edd637fcc4c09cac
simon <simon_brandt@gmx.de>
2022-11-03 15:58:08 +0100

    IMPLEMENT correct initial activeEntity (close enough!)

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,7 +4,7 @@
       <EntitySelector
         v-for="entity in config.entities"
         :entity-type="entity.type"
-        :active="entity.type+'s' === path"
+        :active="entity.type + 's' === activeEntity"
         @entity-click="onEntityClick(entity.type)"
       />
     </div>
@@ -15,45 +15,19 @@
 </template>
 
 <script setup lang="ts">
-import {ref, watch} from "vue";
-import {onBeforeRouteUpdate, useRouter} from 'vue-router'
+import {ref} from "vue";
+import {useRouter} from 'vue-router'
 import {useConfigStore} from "src/stores/config-store";
 import EntitySelector from 'src/components/filters/EntitySelector.vue'
 
 const config = useConfigStore().config
 const router = useRouter()
-// const path = ref(router.currentRoute.value.fullPath.split('/')[1])
 const path = router.currentRoute.value.fullPath.split('/')[1]
-
-onBeforeRouteUpdate(() => {
-    
-})
-
-
 const activeEntity = ref(path)
 
-
-watch(path, () => {
-  console.log('path changed')
-})
-
-
 const onEntityClick = (entityType: string) => {
-
-  console.log('path.value', path.value)
-
-  // const path = router.currentRoute.value.fullPath.split('/')[1]
-  //
-  // console.log('entityType', entityType)
-  //
-  // activeEntity.value = path
-  // // activeEntity.value = entityType
-  // // router.push(`/${entityType}s`)
-  //
-  //
-  // console.log('path', path)
-  // console.log('activeEntity.value', activeEntity.value)
-  // console.log("path === activeEntity.value", path === activeEntity.value)
+  activeEntity.value = entityType + 's'
+  router.push(`/${entityType}s`)
 }
 </script>
 
