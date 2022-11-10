7480eade8d8e2565b7bb4df3d10d2e33c32349fe
simon <simon_brandt@gmx.de>
2022-11-10 17:48:09 +0100

    cleanup 404.vue

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -6,25 +6,7 @@
   </q-page>
 </template>
 <script setup lang="ts">
-import {onMounted, ref} from 'vue'
 import {useConfigStore} from 'stores/config-store'
-import Filters from 'components/filters/Filters.vue';
-import MapView from 'components/map-view/MapView.vue';
-import ListView from 'components/ListView.vue'
-import MapListToggle from 'components/MapListToggle.vue'
 
 const config = useConfigStore().config
-const activeView = ref('map')
-const entitySelected = () => {
-}
-const viewToggled = (newView: string) => {
-  activeView.value = newView
-}
 </script>
-
-<style lang="scss" scoped>
-
-.introduction {
-  margin-bottom: 2em;
-}
-</style>
