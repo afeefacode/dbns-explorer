d6ccce50ea5a6d557cad1048b47b939e1fd7f901
simon <simon_brandt@gmx.de>
2022-11-18 10:28:59 +0100

    ADD config and WORK von Configurator.vue

---
+++ src/assets/configs/all_events.js
@@ -0,0 +1,12 @@
+export const config = {
+  brandColor: '5FA93D',
+  contrastTextColor: 'FFFFFF',
+  entities: [
+    {
+      type: 'events',
+      showFilters: true,
+      showMapView: true,
+      showListView: true,
+    },
+  ]
+}
--- src/pages/Configurator.vue
+++ src/pages/Configurator.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="page-container q-ma-xl">
     <h3>full_widget</h3>
-    <div>{{getSnippet(full_widget)}}</div>
+    <div>{{getSnippet(full_widget, host)}}</div>
     <h3>upcoming_lvns_events</h3>
-    <div>{{getSnippet(upcoming_lvns_events)}}</div>
+    <div>{{getSnippet(upcoming_lvns_events, host)}}</div>
     <h3>all_lvns_events</h3>
-    <div>{{getSnippet(all_lvns_events)}}</div>
+    <div>{{getSnippet(all_lvns_events, host)}}</div>
+    <h3>all_events</h3>
+    <div>{{getSnippet(all_events, host)}}</div>
   </div>
 </template>
 <script setup>
 import {config as full_widget} from 'src/assets/configs/lvns/full_widget'
 import {config as upcoming_lvns_events} from 'src/assets/configs/lvns/upcoming_lvns_events'
 import {config as all_lvns_events} from 'src/assets/configs/lvns/all_lvns_events'
+import {config as all_events} from 'src/assets/configs/all_events'
 import {configJsonToUrlParam} from 'src/utils'
 
-const getSnippet = (config) => {
+// const host = 'http://localhost:9000'
+const host = 'https://dbns-explorer.netlify.app'
+
+const getSnippet = (config, host) => {
   return `<style>
   #iframe-dbns-explorer {
     width: 100%;
@@ -27,7 +33,7 @@ const getSnippet = (config) => {
     display: none;
   }
 </style>
-<iframe id="iframe-dbns-explorer" title="Aktiv in Sachsen" src="http://localhost:9000/?${configJsonToUrlParam(config)}"></iframe>
+<iframe id="iframe-dbns-explorer" title="Aktiv in Sachsen" src="${host}/?${configJsonToUrlParam(config)}"></iframe>
 <script>
   var iFrame = document.getElementById('iframe-dbns-explorer');
 
@@ -36,7 +42,7 @@ const getSnippet = (config) => {
   };
 
   window.addEventListener("message", function (event) {
-    if (event.origin !== "http://localhost:9000") {
+    if (event.origin !== "${host}") {
       return;
     };
 
