ef897d56138d1daac156cc65cd449cee2bb8ed64
simon <simon_brandt@gmx.de>
2022-11-18 12:41:29 +0100

    WORK von Configurator.vue

--- src/pages/Configurator.vue
+++ src/pages/Configurator.vue
@@ -1,24 +1,36 @@
 <template>
   <div class="page-container q-ma-xl">
-    <h3>full_widget</h3>
-    <div>{{getSnippet(full_widget, host)}}</div>
-    <h3>upcoming_lvns_events</h3>
-    <div>{{getSnippet(upcoming_lvns_events, host)}}</div>
-    <h3>all_lvns_events</h3>
-    <div>{{getSnippet(all_lvns_events, host)}}</div>
-    <h3>all_events</h3>
-    <div>{{getSnippet(all_events, host)}}</div>
+    <h5>Host</h5>
+    <div class="q-gutter-sm">
+      <q-radio v-model="host" val="http://localhost:9000" label="Development (localhost)"/>
+      <q-radio v-model="host" val="https://dbns-explorer.netlify.app" label="Production (netlify.app)"/>
+    </div>
+    <div v-for="config in configs">
+      <h3>{{ config.name }}</h3>
+      <div class="snippet" @click="copyToClipboard(getSnippet(config, host))">{{ getSnippet(config, host) }}</div>
+    </div>
   </div>
 </template>
 <script setup>
+import {ref} from 'vue'
 import {config as full_widget} from 'src/assets/configs/lvns/full_widget'
 import {config as upcoming_lvns_events} from 'src/assets/configs/lvns/upcoming_lvns_events'
 import {config as all_lvns_events} from 'src/assets/configs/lvns/all_lvns_events'
 import {config as all_events} from 'src/assets/configs/all_events'
 import {configJsonToUrlParam} from 'src/utils'
 
-// const host = 'http://localhost:9000'
-const host = 'https://dbns-explorer.netlify.app'
+const configs = [
+  full_widget,
+  upcoming_lvns_events,
+  all_lvns_events,
+  all_events
+]
+
+const host = ref('http://localhost:9000')
+
+const copyToClipboard = (text) => {
+  navigator.clipboard.writeText(text.replace(/\s/g,''))
+}
 
 const getSnippet = (config, host) => {
   return `<style>
@@ -65,3 +77,8 @@ const getSnippet = (config, host) => {
   });<\/script>`
 }
 </script>
+<style lang="scss" scoped>
+.snippet {
+  cursor: pointer;
+}
+</style>
