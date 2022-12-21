13bd552b63ac294ba62f34140da32586540d0ffc
simon <simon_brandt@gmx.de>
2022-12-21 15:38:08 +0100

    add MARKERS for all ENTITIES

--- src/components/list/map-view/MapLibre.vue
+++ src/components/list/map-view/MapLibre.vue
@@ -13,6 +13,21 @@ import pngMarkerActive from 'assets/markers/marker-active.png'
 import pngMarkerActors from 'assets/markers/marker-actors.png'
 import pngMarkerActorsActive from 'assets/markers/marker-actors-active.png'
 
+import pngMarkerCounselings from 'assets/markers/marker-counselings.png'
+import pngMarkerCounselingsActive from 'assets/markers/marker-counselings-active.png'
+
+import pngMarkerEducations from 'assets/markers/marker-educations.png'
+import pngMarkerEducationsActive from 'assets/markers/marker-educations-active.png'
+
+import pngMarkerEvents from 'assets/markers/marker-events.png'
+import pngMarkerEventsActive from 'assets/markers/marker-events-active.png'
+
+import pngMarkerProjects from 'assets/markers/marker-projects.png'
+import pngMarkerProjectsActive from 'assets/markers/marker-projects-active.png'
+
+import pngMarkerStores from 'assets/markers/marker-stores.png'
+import pngMarkerStoresActive from 'assets/markers/marker-stores-active.png'
+
 import {useBaseStore} from "src/stores/base-store";
 import {useEntityStore} from "src/stores/entity-store";
 import {getTypeFromEntity, hasLatLong} from 'src/utils'
@@ -46,16 +61,32 @@ const getMarkerPng = (entityType: string) => {
     inactive: '',
     active: ''
   }
+
   switch (entityType) {
     case 'actor':
       marker.inactive = pngMarkerActors
       marker.active = pngMarkerActorsActive
       break;
     case 'project':
+      marker.inactive = pngMarkerProjects
+      marker.active = pngMarkerProjectsActive
+      break;
     case 'event':
+      marker.inactive = pngMarkerEvents
+      marker.active = pngMarkerEventsActive
+      break;
     case 'education':
+      marker.inactive = pngMarkerEducations
+      marker.active = pngMarkerEducationsActive
+      break;
     case 'counseling':
+      marker.inactive = pngMarkerCounselings
+      marker.active = pngMarkerCounselingsActive
+      break;
     case 'store':
+      marker.inactive = pngMarkerStores
+      marker.active = pngMarkerStoresActive
+      break;
     default:
       marker.inactive = pngMarker
       marker.active = pngMarkerActive
