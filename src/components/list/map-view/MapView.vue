4fe8ca969df2b2225a508b401102f4d6da4031b0
simon <simon_brandt@gmx.de>
2023-01-03 15:21:02 +0100

    revert to SEPARATE MAP and LIST VIEW

--- src/components/list/map-view/MapView.vue
+++ src/components/list/map-view/MapView.vue
@@ -2,7 +2,6 @@
   <div class="map-view">
     <div
       class="map-view__map-container"
-      :style="`height: ${mapExpanded ? 600 : 300}px; position: relative`"
     >
       <MapLibre
         @openDetails="openDetails"
@@ -15,14 +14,6 @@
         @closeDetails="closeDetails"
       />
     </div>
-    <div class="row justify-end">
-      <q-btn
-        :icon="mapExpanded ? 'unfold_less' : 'unfold_more'"
-        flat
-        :label="mapExpanded ? 'Karte verkleinern' : 'Karte vergrößern'"
-        @click="mapExpanded = !mapExpanded"
-      />
-    </div>
   </div>
 </template>
 <script setup>
@@ -31,7 +22,6 @@ import MapLibre from 'components/list/map-view/MapLibre.vue'
 import DetailsDrawer from 'components/list/map-view/DetailsDrawer.vue'
 
 const activeEntity = ref(null)
-const mapExpanded = ref(false)
 
 const openDetails = (entity) => {
   activeEntity.value = entity
@@ -48,6 +38,7 @@ const closeDetails = () => {
 
   &__map-container {
     transition: height 100ms ease-in-out;
+    height: 600px;
   }
 }
 
